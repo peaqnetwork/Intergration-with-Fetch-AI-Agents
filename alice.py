664d1132e6ecf57b9095379214a291bede3b8235
lavish0000 <lavick100@gmail.com>
2023-05-23 13:20:22 +0530

    update: get fetch agent balance

--- alice.py
+++ alice.py
@@ -163,6 +163,8 @@ async def check_balance(ctx: Context):
             ctx.logger.info(f"Address mapping not found for {bobAddress}")
             return
         agentAddress = mappedAddress.split(':')[0]
+        fetchAgentBalance = ctx.ledger.query_bank_balance(agentAddress)
+        ctx.logger.info(f"agent's balance: {fetchAgentBalance}")
         await ctx.send(agentAddress, Transfer(amount=(minimum_balance - balance)))
         
     
--- bob.py
+++ bob.py
@@ -161,6 +161,8 @@ async def check_balance(ctx: Context):
             ctx.logger.info(f"Address mapping not found for {aliceAddress}")
             return
         agentAddress = mappedAddress.split(':')[0]
+        fetchAgentBalance = ctx.ledger.query_bank_balance(agentAddress)
+        ctx.logger.info(f"agent's balance: {fetchAgentBalance}")
         await ctx.send(agentAddress, Transfer(amount=(minimum_balance - balance)))
         
     
